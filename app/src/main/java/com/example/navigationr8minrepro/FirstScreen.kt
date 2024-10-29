a6932d37fb0e2b1f0ee1004e3bf676bdbfa69539
Jonathan Stewart <JonathanStewart8301@gmail.com>
2024-10-29 12:11:01 -0500

    Initial Commit

---
+++ app/src/main/java/com/example/navigationr8minrepro/FirstScreen.kt
@@ -0,0 +1,9 @@
+package com.example.navigationr8minrepro
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun FirstScreen() {
+    Text(text = "Hola")
+}
